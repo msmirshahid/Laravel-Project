516e9896b98f4ebcff621bc252dbe33662116977
MS Mir Shahid <44402778+msmirshahid@users.noreply.github.com>
2024-06-25 00:01:49 +0600

    Add files via upload

---
+++ ProfileController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donal Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        $cookie = cookie($cookieName, $cookieValue, $minutes, $path, $domain, $secure, $httpOnly);
+
+        return response()->json($data, 200)->cookie($cookie);
+    }
+}
+
+
+?>
